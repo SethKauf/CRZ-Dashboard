e6f6ee1d1a1e068a8946275834a490c7789c8595
Seth <sethkaufman7@gmail.com>
2026-09-08 22:43:32 -0400

    Adding selected files from database setup branch

---
+++ src/mappings.py
@@ -0,0 +1,24 @@
+REGION_MAPPING = {
+        "Brooklyn": 1,
+        "Queens": 2,
+        "New Jersey": 3,
+        "West Side Highway": 4,
+        "FDR Drive": 5,
+        "West 60th St": 6,
+        "East 60th St": 7
+    }
+
+GROUP_MAPPING = {
+    "Brooklyn Bridge": 1,
+    "Hugh L. Carey Tunnel": 2,
+    "Williamsburg Bridge": 3,
+    "Manhattan Bridge": 4,
+    "Queensboro Bridge": 5,
+    "Queens Midtown Tunnel": 6,
+    "Holland Tunnel": 7,
+    "Lincoln Tunnel": 8,
+    "West Side Highway at 60th St": 9,
+    "FDR Drive at 60th St": 10,
+    "West 60th St": 11,
+    "East 60th St": 12
+}
